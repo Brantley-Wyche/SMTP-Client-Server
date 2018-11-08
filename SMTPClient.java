62571a63cf5298bcb53d07545c67ea519aa5b0ac
Xin Liu <xhliu97@gmail.com>
2018-11-08 08:07:35 -0500

    added uml diagram, removed EventHandler from classes

--- SMTPClient.java
+++ SMTPClient.java
@@ -17,7 +17,7 @@ import javafx.geometry.*;
  * @author Brandon Mok + Xin Liu + Brantley Wyche
  * @version 11/7/18
  */
-public class SMTPClient extends Application implements EventHandler<ActionEvent> {
+public class SMTPClient extends Application {
 	// Attributes
 	// GUI
 	private Stage stage;
@@ -99,6 +99,6 @@ public class SMTPClient extends Application implements EventHandler<ActionEvent>
 	 * The order of commands must all be approved by the server before sending the message
 	 */
 	private void doSend() {
-		
+
 	}
 }
