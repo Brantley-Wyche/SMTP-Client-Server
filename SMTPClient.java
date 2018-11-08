f5be1b248059fe665d9bba38c7f2557a866fd71b
Xin Liu <xhliu97@gmail.com>
2018-11-08 07:26:58 -0500

    updated server code

--- SMTPClient.java
+++ SMTPClient.java
@@ -96,9 +96,9 @@ public class SMTPClient extends Application implements EventHandler<ActionEvent>
 	/**
 	 * Sends command 'MAIL FROM: <address>' then 'RCPT TO: <address>' then 'DATA' before encrypting and sending the message
 	 *
-	 * The order of commands must all be approved by the server before encrypting and sending the message
+	 * The order of commands must all be approved by the server before sending the message
 	 */
 	private void doSend() {
-		// TODO: Ceasar cipher should go here
+		
 	}
 }
